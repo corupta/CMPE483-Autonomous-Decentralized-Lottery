8452e5beb2c10a1d48eb911e39389e0362a8f825
corupta <halitozsoy1584@gmail.com>
2019-12-06 13:47:09 +0300

    remove build directory and extracted submission zip and add them to .gitignore

--- ozsoy/src/migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-const EIP20 = artifacts.require("EIP20");
-const LotteryContract = artifacts.require("LotteryContract");
-
-module.exports = function(deployer) {
-  deployer.deploy(EIP20, 1000000, 'TL Token', 0, 'TL').then(function() {
-    return deployer.deploy(LotteryContract, EIP20.address);
-  });
-};
--- ozsoy/src/test/test1.js
+++
@@ -1,9 +0,0 @@
-const EIP20 = artifacts.require("EIP20");
-
-contract('EIP20', (accounts) => {
-  it('should put 1000000 TL Token in the first account', async () => {
-    const eip20Instance = await EIP20.deployed();
-    const balance = await eip20Instance.balanceOf.call(accounts[0]);
-    assert.equal(balance.valueOf(), 1000000, "1000000 wasn't in the first account");
-  });
-});
